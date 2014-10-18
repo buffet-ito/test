1ce2b89ba7a1d584334fc778cebe05d5d08c5173
buffet-ito <e_mail_takuya@yahoo.co.jp>
2014-10-18 14:59:53 +0900

    unityプロジェクト追加

---
+++ Assets/action.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class action : MonoBehaviour {
+    Material material;
+    Color color = Color.white;
+
+    void Start()
+    {
+        material = Resources.Load<Material>("Materials/TestMaterial");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetThreshold(float threshold)
+    {
+        material.SetFloat("_threshold", threshold);
+    }
+
+    public void SetColor()
+    {
+        material.SetColor("_Color", color);
+    }
+
+    public void SetRed(float r)
+    {
+        color.r = r;
+        SetColor();
+    }
+
+    public void SetGreen(float g)
+    {
+        color.g = g;
+        SetColor();
+    }
+
+    public void SetBlue(float b)
+    {
+        color.b = b;
+        SetColor();
+    }
+}
